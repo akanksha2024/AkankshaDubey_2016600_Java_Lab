ac84605c4d873eecec8d30d4a7de3d9d6105227e
akanksha2024 <100550365+akanksha2024@users.noreply.github.com>
2022-06-13 14:47:15 +0530

    Update 17.java

--- 17.java
+++ 17.java
@@ -1,1 +1,33 @@
-
+import java.util.*;
+import java.util.Scanner;
+class MyCalculator
+{
+int power(int n,int p)throws Exception
+{
+if(n<0||p<0)
+throw new Exception("n or p should not be negative");
+else if(n==0&&p==0)
+throw new Exception("n and p should not be zero");
+return (int)Math.pow((double)n,(double)p);
+}
+}
+class Solution
+{
+public static void main(String []args)
+{
+Scanner in =new Scanner(System.in);
+while(in.hasNextInt())
+{
+int n=in.nextInt();
+int p=in.nextInt();
+MyCalculator M= new MyCalculator();
+try
+{
+System.out.println(M.power(n,p));
+}catch(Exception e)
+{
+System.out.println(e);
+}
+}
+}
+}
